b52fd11b38549437eb82f895e8c17970bbcc128a
eKuuuuu <erik.roigas@metropolia.fi>
2025-09-28 16:19:04 +0300

    updated vite.config, might work now?

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+(async () => {
+  const { defineConfig } = await import('vite');
+  const react = (await import('@vitejs/plugin-react-swc')).default;
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+  module.exports = defineConfig({
+    plugins: [react()],
     server: {
-    host: '0.0.0.0',
-    port: 3000
-  }
-})
-
-
+      host: '0.0.0.0',
+      port: 3000,
+    },
+  });
+})();
