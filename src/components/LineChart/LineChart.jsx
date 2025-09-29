2f0a0f91a974ced0499d2f02268cde9316682daf
RedEyeSH <97534002+RedEyeSH@users.noreply.github.com>
2025-09-29 15:29:50 +0300

    feat: update package.json and package-lock.json with new dependencies; enhance LineChart and Admin styles

--- src/components/LineChart/LineChart.jsx
+++ src/components/LineChart/LineChart.jsx
@@ -2,29 +2,33 @@ import React from "react";
 import { ResponsiveLine } from "@nivo/line";
 
 const LineChart = ({ data }) => (
+  <div style={{ height: "400px", width: "100%" }}>
     <ResponsiveLine
-        data={data}
-        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-        yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
-        axisBottom={{ legend: 'transportation', legendOffset: 36 }}
-        axisLeft={{ legend: 'count', legendOffset: -40 }}
-        pointSize={10}
-        pointColor={{ theme: 'background' }}
-        pointBorderWidth={2}
-        pointBorderColor={{ from: 'seriesColor' }}
-        pointLabelYOffset={-12}
-        enableTouchCrosshair={true}
-        useMesh={true}
-        legends={[
-            {
-                anchor: 'bottom-right',
-                direction: 'column',
-                translateX: 100,
-                itemWidth: 80,
-                itemHeight: 22,
-                symbolShape: 'circle'
-            }
-        ]}
+      data={data}
+      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+      xScale={{ type: "point" }}
+      yScale={{ type: "linear", min: "auto", max: "auto", stacked: true, reverse: false }}
+      axisBottom={{ legend: "transportation", legendOffset: 36, legendPosition: "middle" }}
+      axisLeft={{ legend: "count", legendOffset: -40, legendPosition: "middle" }}
+      pointSize={10}
+      pointColor={{ theme: "background" }}
+      pointBorderWidth={2}
+      pointBorderColor={{ from: "serieColor" }}
+      pointLabelYOffset={-12}
+      enableTouchCrosshair={true}
+      useMesh={true}
+      legends={[
+        {
+          anchor: "bottom-right",
+          direction: "column",
+          translateX: 100,
+          itemWidth: 80,
+          itemHeight: 22,
+          symbolShape: "circle",
+        },
+      ]}
     />
-)
+  </div>
+);
+
 export default LineChart;
